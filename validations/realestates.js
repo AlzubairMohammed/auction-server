7f98ac9f13d4ce000df9f93854074f172a35b699
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-04 22:38:36 +0300

    Add realestates validations

--- validations/realestates.js
+++ validations/realestates.js
@@ -1,12 +1,31 @@
 const { body } = require("express-validator");
 
-exports.auctionsValidator = () => {
+exports.realestateValidator = () => {
   return [
-    body("assignment_number")
+    body("customer_name")
       .notEmpty()
       .withMessage("Field is required .")
       .isString()
       .withMessage("Field must be string ."),
-    body("auction_type").notEmpty().withMessage("Field is required ."),
+    body("owner_name")
+      .notEmpty()
+      .withMessage("Field is required .")
+      .isString()
+      .withMessage("Field must be string ."),
+    body("owner_number")
+      .notEmpty()
+      .withMessage("Field is required .")
+      .isString()
+      .withMessage("Field must be string ."),
+    body("customer_number")
+      .notEmpty()
+      .withMessage("Field is required .")
+      .isString()
+      .withMessage("Field must be string ."),
+    body("auction_id")
+      .notEmpty()
+      .withMessage("Field is required .")
+      .isInt()
+      .withMessage("Field must be integer ."),
   ];
 };
