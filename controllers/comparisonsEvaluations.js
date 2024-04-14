7d2bb036c7dd226814e81f739e73de03d45772f0
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-14 15:23:12 +0300

    Add create comparsons logic

--- controllers/comparisonsEvaluations.js
+++ controllers/comparisonsEvaluations.js
@@ -3,7 +3,12 @@ const { models } = require("../database/connection");
 const httpStatus = require("../utils/httpStatus.js");
 const errorResponse = require("../utils/errorResponse");
 const { validationResult } = require("express-validator");
-const { comparisons_evaluations } = models;
+const {
+  comparisons_evaluations,
+  comparisons_evaluation_properties,
+  comparisons_evaluation_realestates,
+  comparisons_evaluation_realestates_properties,
+} = models;
 
 exports.getComparisonsEvaluations = asyncWrapper(async (req, res) => {
   let data = await comparisons_evaluations.findAll();
@@ -18,13 +23,14 @@ exports.getComparisonsEvaluation = asyncWrapper(async (req, res) => {
 });
 
 exports.createComparisonsEvaluation = asyncWrapper(async (req, res, next) => {
-  let multiArray = req.body;
+  let comparisons = req.body.comparisons;
+  let properties = req.body.properties;
   let result = 0;
-  let counter1 = multiArray.length;
-  let counter2 = multiArray[0].length;
+  let counter1 = comparisons.length;
+  let counter2 = comparisons[0].length;
   while (counter1--) {
     let nestedCounter = counter2;
-    let realestateItem = [...multiArray[counter1]];
+    let realestateItem = [...comparisons[counter1]];
     while (nestedCounter--) {
       if (nestedCounter) {
         if (nestedCounter === 1) {
@@ -34,21 +40,54 @@ exports.createComparisonsEvaluation = asyncWrapper(async (req, res, next) => {
             100;
         } else {
           realestateItem[0].percentage +=
-            (multiArray[counter1][0].percentage *
+            (comparisons[counter1][0].percentage *
               realestateItem[nestedCounter - 1].percentage) /
             100;
         }
       }
     }
   }
-  return res.json({ status: httpStatus.SUCCESS, result });
+  result = result.toFixed(2);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = errorResponse.create(errors.array(), 400, httpStatus.FAIL);
     return next(error);
   }
-  let data = await comparisons_evaluations.create(req.body);
-  return res.json({ status: httpStatus.SUCCESS, data });
+  // looping in properties array to comparisons_evaluation_properties
+  let propertiesData;
+  properties.forEach(async (propertie) => {
+    propertiesData = await comparisons_evaluation_properties.create({
+      name: propertie,
+    });
+  });
+  let comparisonsEvaluationsData = await comparisons_evaluations.create({
+    realestate_id: req.body.realestate_id,
+    meter_price: result,
+  });
+  let comparisonsEvaluationRealestatesData =
+    await comparisons_evaluation_realestates.create({
+      comparisons_evaluation_id: comparisonsEvaluationsData.id,
+    });
+
+  // looping in two dicmensions array
+  let counter = comparisons.length;
+  while (counter--) {
+    let nestedCounter = comparisons[counter].length;
+    while (nestedCounter--) {
+      await comparisons_evaluation_realestates_properties.create({
+        comparisons_evaluation_realestate_id:
+          comparisonsEvaluationRealestatesData.id,
+        comparisons_evaluation_properties_id: propertiesData.id,
+        value: comparisons[counter][nestedCounter].value,
+        percentage: comparisons[counter][nestedCounter].percentage,
+      });
+    }
+  }
+
+  return res.json({
+    status: httpStatus.SUCCESS,
+    data: comparisonsEvaluationsData,
+  });
 });
 
 exports.editComparisonsEvaluation = asyncWrapper(async (req, res) => {
