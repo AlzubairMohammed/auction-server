ecbd704c981f3e1d6cab66c92da562d3757c24fe
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-23 08:46:40 +0300

    Add create realestate document

--- controllers/realestates.js
+++ controllers/realestates.js
@@ -9,13 +9,15 @@ const {
   realestate_owners,
   realestate_licenses,
   realestate_documents,
+  realestate_files,
 } = models;
 
 exports.createRealestate = asyncWrapper(async (req, res, next) => {
   req.body = convertFormData(req.body);
   req.body.owners = Object.values(req.body.owners);
-  req.body.files = Object.values(req.body.files);
-  req.files = Object.values(req.files)[0];
+  req.files = Object.values(req.files);
+  const fileNames = req.body.filesNames;
+  let counter = 0;
   // return res.json({ status: httpStatus.SUCCESS, data: req.body });
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -23,9 +25,54 @@ exports.createRealestate = asyncWrapper(async (req, res, next) => {
     return next(error);
   }
   const { owners, document } = req.body;
-  console.log(typeof owners, owners);
   // create realestate
+
   let data = await realestates.create(req.body);
+  // upload files
+  if (req.files) {
+    if (Array.isArray(req.files)) {
+      await Promise.all(
+        req.files.map(async (file) => {
+          await realestate_files.create({
+            realestate_id: data.id,
+            name: fileNames[counter++].name,
+            path: file.name,
+          });
+          const filePath = `uploads/realestates/${file.name}`;
+          await file.mv(filePath);
+        })
+      );
+    } else {
+      const file = req.files;
+      await realestate_files.create({
+        realestate_id: data.id,
+        name: fileNames[counter++].name,
+        path: file.name,
+      });
+    }
+  }
+  // create realestate document
+  if (document) {
+    await realestate_documents.create({
+      realestate_id: data.id,
+      number: document.number,
+      area_number: document.area_number,
+      block_number: document.block_number,
+      graph_number: document.graph_number,
+      space: document.space,
+      quarter_id: document.quarter_id,
+      north_desc: document.north_desc,
+      north_space: document.north_space,
+      west_desc: document.west_desc,
+      west_space: document.west_space,
+      east_desc: document.east_desc,
+      east_space: document.east_space,
+      south_desc: document.south_desc,
+      south_space: document.south_space,
+      note: document.note,
+      path: document.path,
+    });
+  }
   // create realestate license
   req.body.license.realestate_id = data.id;
   await realestate_licenses.create(req.body.license);
@@ -62,6 +109,14 @@ exports.getRealestate = asyncWrapper(async (req, res) => {
       {
         model: realestate_licenses,
         as: "realestate_licenses",
+      },
+      {
+        model: realestate_documents,
+        as: "realestate_documents",
+      },
+      {
+        model: realestate_files,
+        as: "realestate_files",
       },
     ],
   });
