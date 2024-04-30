47a386289823d0cfd4b381cfc527ec14b3b05e55
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-30 12:54:52 +0300

    Add all cost evaluation equations

--- controllers/costEvaluations.js
+++ controllers/costEvaluations.js
@@ -25,12 +25,62 @@ exports.getCostEvaluation = asyncWrapper(async (req, res) => {
 });
 
 exports.createCostEvaluation = asyncWrapper(async (req, res, next) => {
-  const { directCostOperations } = req.body;
+  const { directCostOperations, indirectCostOperations, depreciationRate } =
+    req.body;
+  let directCostTotal = 0;
+  let indirectCostTotal = 0;
+  let depreciationRateValue = 0;
+  let depreciationCost = 0;
+  let realestateCostAfterDepreciation = 0;
   let sum = 0;
   for (const item of directCostOperations) {
     sum += item?.meter_price * item?.area;
   }
-  return res.json({ status: httpStatus.SUCCESS, sum });
+  directCostTotal = sum;
+  sum = 0;
+
+  for (const item of indirectCostOperations) {
+    if (item.costType === "percentage") {
+      sum += (directCostTotal / 100) * +item.precentage;
+    } else {
+      sum += +item?.precentage;
+    }
+  }
+  indirectCostTotal = sum;
+  realestateCostAfterDepreciation = 0;
+
+  if (depreciationRate.type === "const") {
+    depreciationRateValue =
+      (+depreciationRate.realestate_life_span /
+        +depreciationRate.realestate_expected_life_span) *
+      100;
+    depreciationRateValue = Math.round(depreciationRateValue);
+    depreciationCost =
+      ((indirectCostTotal + directCostTotal) * depreciationRateValue) / 100;
+    realestateCostAfterDepreciation =
+      indirectCostTotal + directCostTotal - depreciationCost;
+  } else {
+    depreciationRateValue =
+      (+depreciationRate.realestate_expanded_life_span /
+        +depreciationRate.realestate_expected_life_span) *
+      100;
+    depreciationRateValue = Math.round(depreciationRateValue);
+    depreciationCost =
+      ((indirectCostTotal + directCostTotal) * depreciationRateValue) / 100;
+    realestateCostAfterDepreciation =
+      indirectCostTotal + directCostTotal - depreciationCost;
+  }
+
+  return res.status(500).json({
+    status: httpStatus.SUCCESS,
+    directCostOperations,
+    indirectCostOperations,
+    directCostTotal,
+    indirectCostTotal,
+    depreciationRateValue,
+    depreciationCost,
+    realestateCostAfterDepreciation,
+  });
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
